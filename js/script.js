a3c4b812395d279171f8e9e3896f15e8044e8b77
Tanya <ts.tatyana25@gmail.com>
2022-05-14 18:24:22 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+})
